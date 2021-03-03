3476aa4ecb43ee0c80877b539bcdcdc02bd4de3f
Victoria Wika <victoriawika@icloud.com>
2021-03-03 12:11:05 +0100

    Add routes for /api/users and /api/cards

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
-    console.log(req.method, req.url)
-    res.end('Hello world')
+app.use('/api/users', (req, res) => {
+  res.end('[{"name": "Melissa", "role": "student"}]')
+})
+// lange Schreibweise
+// app.use((req, res, next) => {
+//     req.url === '/api/users'
+//     ? res.end('[{"name": "Melissa", "role": "student"}]')
+//     : next()
+// })
+
+app.use('/api/cards', (req, res) => {
+  res.end('[{"title": "First card"}]')
 })
 
-
 app.listen(3000, () => {
-    console.log('Server startet at http://localhost:3000')
+  console.log('Server startet at http://localhost:3000')
 })
