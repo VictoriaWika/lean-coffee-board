c29ff39be796988654f0ad65da138b0b8432507e
Victoria Wika <victoriawika@icloud.com>
2021-03-03 15:52:20 +0100

    Add models + mongoose

--- server.js
+++ server.js
@@ -2,17 +2,28 @@ const express = require('express')
 const app = express()
 let users = []
 const { v4: v4 } = require('uuid')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+
+mongoose
+  .connect('mongodb://localhost/lean-coffee-board', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to mongodb'))
+  .catch(error => console.error('Could not connect to mongodb', error))
 
 app.use(express.json()) // add middleware for json data
 
-app.get('/api/users', (req, res) => {
-  res.json(users)
+app.get('/api/users', async (req, res) => {
+  // Jerrys Kommentar
+  res.json(await User.find())
 })
 
-app.get('/api/users/:id', (req, res) => {
+app.get('/api/users/:id', async (req, res) => {
   // :id, weil es ein variabler Wert ist, den wir austauschen
   const { id } = req.params
-  res.json(users.find(user => user.id === id))
+  res.json(await User.findOne({ id }))
 })
 
 // ------------------------------------------
@@ -26,10 +37,10 @@ app.delete('/api/users/:id', (req, res) => {
 
 // ------------------------------------------
 
-app.post('/api/users', (req, res) => {
-  const newUser = { ...req.body, id: v4() }
-  users.push(newUser)
-  res.json(newUser)
+app.post('/api/users', async (req, res) => {
+  //   const newUser = { ...req.body, id: v4() }
+  //   users.push(newUser)
+  res.json(await User.create(req.body))
 })
 
 app.get('/api/cards', (req, res) => {
