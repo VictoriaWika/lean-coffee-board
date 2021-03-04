11a10faea048e4d3871c6758b37004a178ef817a
Victoria Wika <victoriawika@icloud.com>
2021-03-04 21:26:32 +0100

    Add globalstyle

---
+++ client/src/GlobalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 20px;
+  font-size: 112.5%;
+  font-family: sans-serif;
+}
+input,
+button {
+  font-size: inherit;
+  padding: 5px;
+}
+`
