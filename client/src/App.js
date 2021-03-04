0feb9529f424b9807cf63e7f585bccc379304814
Victoria Wika <victoriawika@icloud.com>
2021-03-04 15:30:55 +0100

    Add form to create a card

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,57 @@
 import { useState, useEffect } from 'react'
+import createCard from './services/createCard'
+import getCards from './services/getCards'
+import getUsers from './services/getUsers'
 
 function App() {
   const [users, setUsers] = useState([])
+  const [cards, setCards] = useState([])
+
   useEffect(() => {
-    fetch('/api/users')
-      .then(res => res.json())
-      .then(data => setUsers([...data]))
+    getUsers().then(data => setUsers([...data]))
   }, [])
+
+  useEffect(() => {
+    getCards().then(data => setCards([...data]))
+  }, [])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const { text, author } = form.elements
+    createCard({ text: text.value, author: author.value }).then(() =>
+      getCards().then(data => setCards([...data]))
+    )
+  }
 
   return (
     <div>
+      <h2>Users:</h2>
       {users.map(user => (
-        <div key={user._id}>{user.name}</div>
+        <div key={user._id}>
+          {user.name} ({user._id})
+        </div>
+      ))}
+      <h2>Cards:</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Text:
+          <br />
+          <input name="text" />
+        </label>
+        <label>
+          Author ID:
+          <br />
+          <input name="author" />
+        </label>
+        <button>Create card</button>
+      </form>
+      {cards.map(card => (
+        <div key={card._id}>
+          {card.text} ({card.author.name})
+        </div>
       ))}
     </div>
   )
 }
-
 export default App
