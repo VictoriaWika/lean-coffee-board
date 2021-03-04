9f9d9738848fabeed514ee2a71af44d5a661d8b7
Victoria Wika <victoriawika@icloud.com>
2021-03-04 14:26:41 +0100

    Add react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('/api/users')
+      .then(res => res.json())
+      .then(data => setUsers([...data]))
+  }, [])
+
+  return (
+    <div>
+      {users.map(user => (
+        <div key={user._id}>{user.name}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App
