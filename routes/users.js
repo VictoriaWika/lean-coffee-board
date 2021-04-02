c640fbcf261ea5738c613fb338f50c0f983fc1db
Victoria Wika <victoriawika@icloud.com>
2021-04-02 21:15:30 +0200

    Cleanup

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,20 @@ const User = require('../models/User')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  // Jerrys Kommentar
   res.json(await User.find().catch(next))
 })
 
 router.get('/:id', async (req, res, next) => {
-  // :id, weil es ein variabler Wert ist, den wir austauschen
   const { id } = req.params
   res.json(await User.findById(id).catch(next))
 })
 
 router.delete('/:id', async (req, res, next) => {
-  //   const index = users.findIndex(user => user.id === id)
-  //   users = [...users.slice(0, index), ...users.slice(index + 1)]
-  //   res.json(users)
   const { id } = req.params
   res.json(await User.findByIdAndDelete(id).catch(next))
 })
 
 router.post('/', async (req, res, next) => {
-  //   const newUser = { ...req.body, id: v4() }
-  //   users.push(newUser)
   res.json(await User.create(req.body).catch(next))
 })
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const setupMongo = require('./setupMongo')
 setupMongo()
 const app = express()
 
-app.use(express.json()) // add middleware for json data
+app.use(express.json())
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/cards', require('./routes/cards'))
@@ -13,15 +13,3 @@ app.use(require('./routes/error'))
 app.listen(4000, () => {
   console.log('Server started at http://localhost:4000')
 })
-
-// Vor res.json sah unser code so aus:
-// app.use('/api/users', (req, res) => {
-//     res.end('[{"name": "Melissa", "role": "student"}]')
-//   })
-
-// lange Schreibweise für app.use
-// app.use((req, res, next) => {
-//     req.url === '/api/users'
-//     ? res.end('[{"name": "Melissa", "role": "student"}]')
-//     : next()
-// })
